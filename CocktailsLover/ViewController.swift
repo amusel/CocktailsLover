e127edf945e7f9e74708c831ee6861d28329ceae
amusel <a.musel98@gmail.com>
2020-09-15 20:39:05 +0300

    Initial Commit

---
+++ CocktailsLover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailsLover
+//
+//  Created by Artem Musel on 15.09.2020.
+//  Copyright © 2020 Artem Musel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
